791af051402e66e935a38be2f4b54d7e31bc4e4c
groupforspeed <telegram.bot4799@gmail.com>
2016-02-27 11:36:24 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -216,7 +216,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "•••Group settings⚙\n•°•°•kick group join with link :"..settings.lock_join.."\n°•°•°Lock group fosh : "..settings.antitag.."\n•°•°•Lock group ads : "..settings.antilink.."\n°•°•°Lock group name : "..settings.lock_name.."\n•°•°•Lock group photo : "..settings.lock_photo.."\n°•°•°kick new member : "..settings.lock_member.."\n•°•°•Lock group leave ban : "..leave_ban.."\n°•°•°flood set on : "..NUM_MSG_MAX.."\n•°•°•Bot Security : "..bots_protection.."                       •••TeleSniper [people-version] v2.1 (◉)"
+  local text = "•••Group settings⚙\n•°•°•kick group join with link :"..settings.lock_join.."\n°•°•°Lock group fosh : "..settings.antitag.."\n•°•°•Lock group ads : "..settings.antilink.."\n•°•°•lock group username :"..settings.lock username.."\n°•°•°Lock group name : "..settings.lock_name.."\n•°•°•Lock group photo : "..settings.lock_photo.."\n°•°•°kick new member : "..settings.lock_member.."\n•°•°•Lock group leave ban : "..leave_ban.."\n°•°•°flood set on : "..NUM_MSG_MAX.."\n•°•°•Bot Security : "..bots_protection.."                       •••TeleSniper [people-version] v2.1 (◉)"
   return text
 end
 
