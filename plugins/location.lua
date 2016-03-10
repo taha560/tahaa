06d74ba291fd09d65d50f8de9bfe9146b68cec24
groupforspeed <telegram.bot4799@gmail.com>
2016-03-10 21:57:26 +0330

    Update location.lua

--- plugins/location.lua
+++ plugins/location.lua
@@ -54,7 +54,7 @@ end
 return {
   description = "Gets information about a location, maplink and overview", 
   usage = "!loc (location): Gets information about a location, maplink and overview",
-  patterns = {"^!loc (.*)$"}, 
+  patterns = {"^(loc) (.*)$"}, 
   run = run 
 }
 
