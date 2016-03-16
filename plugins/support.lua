129be1588224a4b5e722c5f1136db918efafb7dc
groupforspeed <telegram.bot4799@gmail.com>
2016-03-16 21:33:55 +0330

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,35 +1,35 @@
 do
-
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
-end
-
-local function run(msg, matches)
-  local user = 67559249
-
-  if matches[1] == "support" then
-    user = 'user#id'..user
-  end
-
-  -- The message must come from a chat group
-  if msg.to.type == 'chat' then
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "sudo added to group"
-  else 
-    return 'This isnt a chat group!'
-  end
-
-end
-
- -return {
- -  description = "support", 
- -  patterns = {
- -    "^[!/](support)$",
-      "^(support)$"
- -  }, 
- -  run = run 
- -}
- -
- -end
+ 
+ local function callback(extra, success, result)
+   vardump(success)
+   vardump(result)
+ end
+ 
+ local function run(msg, matches)
+   local user = 67559249
+ 
+   if matches[1] == "support" then
+     user = 'user#id'..user
+   end
+ 
+   -- The message must come from a chat group
+   if msg.to.type == 'chat' then
+     local chat = 'chat#id'..msg.to.id
+     chat_add_user(chat, user, callback, false)
+     return "sudo added to group\n"..text.chat.id
+   else 
+     return 'This isnt a chat group!'
+   end
+ 
+ end
+ 
+ return {
+   description = "support", 
+   patterns = {
+     "^[!/](support)$",
+     "^(support)$"
+   }, 
+   run = run 
+ }
+ 
+ end
