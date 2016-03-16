dcaade7a3d5f3b9ac1db84712fa88d677fb1018e
groupforspeed <telegram.bot4799@gmail.com>
2016-03-16 18:41:31 +0330

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,6 +1,8 @@
 local function run(msg, matches ) 
+  local user = 'user#id'..msg.to.id
+local chat = 'chat#id'..msg.from.id
   if matches[1] == "me" then
-    return "آيدي شما\n "..msg.from.id
+    return "آيدي شما\n "..msg.from.id\n..msg.to.id
   end
 end
 
