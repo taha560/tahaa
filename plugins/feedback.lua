83b02927f87c1689b43c567af5b707db1f87354b
groupforspeed <telegram.bot4799@gmail.com>
2016-03-17 14:53:49 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,27 +1,18 @@
 do
  function run(msg, matches)
- 
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
+ local fuse = 'پیام جدیدی دریافت شد !!\n\nنام کاربریش: @''\n\nآی دی کاربر : ' .. msg.from.id .. '\n\nای دی گروه : ' .. msg.to.id .. '\n\nاسم کاربر: ' .. msg.from.print_name .. '\n\nنظر:\n\n'  .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
- 
- 
-   local text = matches[1]
-   local chat = "chat#id"..YourChatId 
-   --like : local chat = "chat#id"..12345678
-   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Sent!'
- 
+ local text = matches[1]
+ local sends = send_msg('user#id67559249', fuse, ok_cb, false)
+ return 'نظرات شما به درستی ارسال شد'
  end
- end
- return {
-  
-  description = "Feedback",
- 
-  usage = "!feedback message",
-  patterns = {
-  "^[Ff]eedback (.*)$"
- 
-  },
-  run = run
- }
+end
+return {
+description = "Feedback",
+usage = "feedback message",
+patterns = {
+ "^[!/#](feedback)(.*)$",
+ "^(feedback)(.*)$"
+ },
+ run = run
+}
